af974f48c9da4355ea6d920054a976b9aceaa1ac
airwick989 <73313597+airwick989@users.noreply.github.com>
2020-12-04 13:00:42 -0500

    h

--- bookSeats.php
+++ bookSeats.php
@@ -72,7 +72,7 @@ $session_value=(isset($_SESSION['username']))?$_SESSION['username']:'';
 										if ($row_seats[$i] == 0) {
 											echo '<td><input class="free-seat" onclick="checkSeatNum(event, this.id);" type="checkbox" style="display: none;" id="'.'r'.$num_row.'c'.($i+1).'" name="seats[]" value="'.'r'.$num_row.'c'.($i+1).'"><label for="'.'r'.$num_row.'c'.($i+1).'"></label></td>';
 										} else if($row_seats[$i] == 1){
-											echo '<td><input type="checkbox" style="display: none;" id="'.'r'.$num_row.'c'.($i+1).'" name="seats[]" value="occupied" onclick="return false;" checked><label for="'.'r'.$num_row.'c'.($i+1).'"></label></td>';
+											echo '<td><input type="checkbox" style="display: none;" id="'.'r'.$num_row.'c'.($i+1).'" name="" value="" onclick="return false;" checked><label for="'.'r'.$num_row.'c'.($i+1).'"></label></td>';
 										} else{
 											echo '<td><input type="checkbox" style="display: none;" id="'.'r'.$num_row.'c'.($i+1).'" name="seats[]" value="'.'r'.$num_row.'c'.($i+1).'" disabled><label for="'.'r'.$num_row.'c'.($i+1).'"><img class="covid" src="images/covidChair.png" height="30" width="30"/></label></td>';
 										}
